2accb7cb19250776caa9c483a4599d63f9326e16
Yomna1987 <132199825+Yomna1987@users.noreply.github.com>
2025-04-17 10:10:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var express = require ('express');
-var app = express ();
-//Serves files from the root directory
-app.use (express.static('./'));
-app.listen(8080, function () {
-	console.log('Listening at http ://localhost:8080');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 8080; // Port dynamique pour OpenShift
+
+// Sert les fichiers statiques depuis le dossier racine
+app.use(express.static('./'));
+
+app.listen(port, function () {
+  console.log(`Listening on http://localhost:${port}`);
 });
